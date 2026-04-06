0b76702137081aa6a0688cd5c57a51554934f348
juniorquiz <multanivicky508@gmail.com>
2026-04-06 13:49:59 -0400

    Correct database URL format in firebaseConfig
    Fixed formatting of the database URL in firebaseConfig.

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAdP2Ifheihh-MW9HXzWKpw0nEqQ_6sarA",
   authDomain: "juniorquizlive.firebaseapp.com",
-  https://juniorquizlive-default-rtdb.firebaseio.com/
+  "https://juniorquizlive-default-rtdb.firebaseio.com",
   projectId: "juniorquizlive",
   storageBucket: "juniorquizlive.firebasestorage.app",
   messagingSenderId: "297942978278",

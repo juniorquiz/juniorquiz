669c6a572d7bb98604d3073941dc20256d52a374
Gurpreet Singh <gora@mac.mynetworksettings.com>
2026-04-06 12:18:54 -0400

    update firbase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdP2Ifheihh-MW9HXzWKpw0nEqQ_6sarA",
+  authDomain: "juniorquizlive.firebaseapp.com",
+  projectId: "juniorquizlive",
+  storageBucket: "juniorquizlive.firebasestorage.app",
+  messagingSenderId: "297942978278",
+  appId: "1:297942978278:web:dc0e05daa95a2dbdb7f527",
+  measurementId: "G-ZGN7GS6CTG"
+};

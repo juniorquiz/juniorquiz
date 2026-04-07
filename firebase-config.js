16e1875a081808550b021e063ebb37a6fd71858d
juniorquiz <multanivicky508@gmail.com>
2026-04-07 00:29:44 -0400

    Initialize Firebase and set up analytics
    Added Firebase initialization and analytics setup.

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAdP2Ifheihh-MW9HXzWKpw0nEqQ_6sarA",
   authDomain: "juniorquizlive.firebaseapp.com",
   databaseURL: "https://juniorquizlive-default-rtdb.firebaseio.com",
@@ -8,3 +16,7 @@ export const firebaseConfig = {
   appId: "1:297942978278:web:dc0e05daa95a2dbdb7f527",
   measurementId: "G-ZGN7GS6CTG"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

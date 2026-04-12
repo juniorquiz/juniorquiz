185af7b81db6f165e687ad7b3278effcd5aed01c
Gurpreet Singh <gora@mac.mynetworksettings.com>
2026-04-12 10:40:09 -0400

    check

--- firebase-config.js
+++ firebase-config.js
@@ -11,13 +11,6 @@ const firebaseConfig = {
   storageBucket: "juniorquizlive.firebasestorage.app",
   messagingSenderId: "297942978278",
   appId: "1:297942978278:web:dc0e05daa95a2dbdb7f527",
-  measurementId: "G-ZGN7GS6CTG"
+  measurementId: "G-ZGN7GS6CTG",
+  databaseURL: "https://juniorquizlive-default-rtdb.firebaseio.com"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Export these so your Register page can use them
-export const auth = getAuth(app);
-export const db = getFirestore(app);
